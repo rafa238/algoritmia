cd4796b32818b5436bbb339d674071c8e621a4e0
rafa238 <rafalaureano246@gmail.com>
2021-11-19 20:08:28 -0600

    ArrayManipulation Agregado

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -97,3 +97,36 @@ void stonesOnTable(){
     cout<<cont;
 }
 */
+/*
+void arrayManupulation(){
+    int n, m;
+    cin>> n >> m;
+    //cout<<n<<m;
+    int abk[m][3];
+    for(int i=0; i<m; i++){
+        for(int j=0; j<3; j++){
+            cin>>abk[i][j];
+        }
+    }
+    int arr[m][n]={};
+
+    for(int i=0;i<m; i++){
+        int a=abk[i][0], b=abk[i][1], c=abk[i][2];
+        for(int j=0; j<n; j++){
+            if(j+1>=a && j+1<=b){
+                //cout<<"entro"<<a<<b;
+                arr[i][j]+=c;
+            }
+            if(i+1<m) arr[i+1][j] = arr[i][j];
+            //cout<<arr[i][j]<<" ";
+        }
+        //cout<<"\n";
+    }
+    int max=arr[0][0];
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(max < arr[i][j]) max=arr[i][j];
+        }
+    }
+    cout<<max;
+}*/
