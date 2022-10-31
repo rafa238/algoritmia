e1e9377e224789c46c811fecfd934119831acc93
rafa238 <rafalaureano246@gmail.com>
2022-10-31 12:34:42 -0600

    Finally get an AC in Elliminating ballons

--- icpc/3th/E.cpp
+++ icpc/3th/E.cpp
@@ -1,19 +1,20 @@
+//https://codeforces.com/gym/103960/problem/E
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     long long int n; cin>>n;
-    //map<int, bool> arrows;
-    long long int cont = 0;
     vector<int> arrows(10e6+1, 0);
     for(long long int i=0; i<n; i++){
         long long int h; cin>>h;
-        if(arrows[h+1] == 0){
-            arrows[h] = 1;
-            cont++;
+        if(arrows[h+1] != 0){
+            arrows[h+1]--;
         }
-        arrows[h+1] = 0;
-        arrows[h] = 1;
+        arrows[h]++;
+    }
+    long long int cont = 0;
+    for(long long int i=0; i<arrows.size(); i++){
+        cont += arrows[i];
     }
     cout<<cont<<endl;
 }
