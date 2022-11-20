b11853cdfedf6db726c72d40910cbac17ebe9923
rafa238 <rafalaureano246@gmail.com>
2022-11-19 20:50:56 -0600

    word_capital added

---
+++ ad-hoc/codeforces/word_capitalization.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/contest/281/problem/A
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    string wortwortwort;
+    cin>>wortwortwort;
+    wortwortwort[0] = toupper(wortwortwort[0]);
+    cout<<wortwortwort<<endl;
+    return 0;
+}
