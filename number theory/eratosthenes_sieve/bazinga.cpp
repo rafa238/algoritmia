41503c968d54354ed416d1aad4019c5b918ef869
rafa238 <rafalaureano246@gmail.com>
2022-12-18 21:25:24 -0600

    bazinga finished :D

--- number theory/eratosthenes_sieve/bazinga.cpp
+++ number theory/eratosthenes_sieve/bazinga.cpp
@@ -1,36 +1,56 @@
+//https://www.spoj.com/problems/DCEPC505/
 #include <bits/stdc++.h>
 using namespace std;
-vector<bool> isPrime;
-vector<int> primes;
-vector<int> n_primes;
-vector<int> almost_primes;
 
-void criba(int n) {
-    isPrime = vector<bool>(n, true);
-    primes = vector<int>();
-    n_primes = vector<int>(n*n, 0);
-    isPrime[0] = isPrime[1] = false;
-    for (int i=2; i<=n; i++) {
-        if (isPrime[i]) {
-            primes.push_back(i);
-            for (int h=2; h*i<=n; h++) {
-                isPrime[i*h] = false;
-                n_primes[i*h]++;
-            }
+const int maxn = 10600000;
+
+vector<int> primos;
+vector<int> criba_lineal(int n){
+    vector<int> lp(n + 1, 0);
+    for(int i = 2; i <= n; i++){
+        if(lp[i] == 0){
+            lp[i] = i;
+            primos.push_back(i);
         }
-        if(n_primes[i] == 2) almost_primes.push_back(i);
+        for(int j=0; primos[j] * i <= n; j++){
+            lp[ i * primos[j]] = primos[j];
+            if( primos[j] == lp[i] )
+                break;
+        }
     }
+    return lp;
 }
+
+vector<int> semiprimos;
+void noDivisors(vector<int> &lp){
+    for(int in=2; in<maxn; in++){
+        int p1, p2, i=in;
+        p1 = lp[i];
+        //cout<<i<<" : "<<p1<<" ";
+        i /= lp[i];
+        if(i == 1) {
+            //cout<<endl; 
+            continue;
+        }
+        p2 = lp[i];
+        i /= lp[i];
+        //cout<<p2<<endl;
+        if(i == 1 && p1 != p2){
+            //cout<<"push"<<endl;
+            semiprimos.push_back(in);
+        }
+    }
+} 
 
 int main(){
     int tc; cin>>tc;
-    int maxn =  2000000;
-    criba(maxn);
+    vector<int> lp = criba_lineal(maxn);
+    noDivisors(lp);
+    //cout<<semiprimos.size();
     while(tc--){
         int n;
         cin>>n;
-        
-        cout<<almost_primes[n]<<endl;
+        cout<<semiprimos[n-1]<<endl;
     }
     
     return 0;
