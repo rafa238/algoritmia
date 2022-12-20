9e58186d0bf4d33896cd067339f0445d72a50741
rafa238 <rafalaureano246@gmail.com>
2022-12-19 22:36:16 -0600

    A+B cd problem finished

---
+++ ad-hoc/codeforces/3_div/A+B.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/contest/1772/problem/A
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int tc;cin>>tc;
+    int a, b;
+    while (tc--){
+        scanf("%d+%d", &a, &b);
+        cout<<a+b<<endl;
+    }
+}
