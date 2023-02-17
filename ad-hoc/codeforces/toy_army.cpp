b6782b2ea6787ff821b1b84836443804a92630f0
rafa238 <rafalaureano246@gmail.com>
2023-02-16 19:08:33 -0600

    toy army problem finished :D

---
+++ ad-hoc/codeforces/toy_army.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+//https://codeforces.com/contest/84/problem/A
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ll n; 
+    cin >> n;
+    cout<< (3*n)/2 << endl;
+    return 0;
+}
