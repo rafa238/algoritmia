1269add20aaecfa34b2513890083eccec14b221a
rafa238 <rafalaureano246@gmail.com>
2023-04-28 02:07:17 -0600

    disjoint sets template finished added

--- conjuntos disjuntos/uva/template.cpp
+++ conjuntos disjuntos/uva/template.cpp
@@ -4,11 +4,15 @@ typedef vector<int> vi;
 class UnionFind {
     //p -> parent respectivly
     //rank -> height of each conjunt
-    private: vi p, rank;
+    private: 
+        vi p, rank, sizes;
+        int numSets;
     public:
         UnionFind(int N){
             rank.assign(N,0);
             p.assign(N, 0);
+            sizes.assign(N, 1);
+            numSets = N;
             for(int i=0; i<N; i++){
                 p[i] = i;
             }
@@ -19,14 +23,20 @@ class UnionFind {
         bool isSameSet(int i, int j){
             return findSet(i) == findSet(j);
         }
+        int sizeOfSet(int i){
+            return sizes[findSet(i)];
+        }
+        int numDijointSets(){
+            return numSets;
+        }
         void unionSet(int i, int j){
             if(!isSameSet(i, j)){
                 int x = findSet(i) , y = findSet(j);
-                if(rank[x] > rank[y]) p[y] = x;
-                else {
-                    p[x] = y;
-                    if(rank[x] == rank[y]) rank[y]++;
-                }
+                if (rank[x] > rank[y]) swap(x, y); //x is the lower set
+                p[x] = y;
+                if (rank[x] == rank[y]) ++rank[y];
+                sizes[y] += sizes[x];
+                numSets--;                  
             }
         }
 };
