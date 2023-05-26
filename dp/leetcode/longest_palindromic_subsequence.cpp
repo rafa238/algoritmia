7de6131fe0815b6e357b7677a40495aef888f508
rafa238 <rafalaureano246@gmail.com>
2023-05-26 13:33:47 -0600

    longest palindromic subsequence finished

---
+++ dp/leetcode/longest_palindromic_subsequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int L = 0, R = 0, ansSize = 0, n = s.size();
+        //odd lenght
+        for(int i=1; i<n; i++){
+            int currSize = 1;
+            for(int j=i+1, k=i-1; j<n && k>=0; j++, k--){
+                if(s[k] != s[j]) break;
+                currSize+=2;
+                if(currSize >= ansSize){
+                    L = k;
+                    R = j;
+                    ansSize = currSize;
+                }
+            }
+        }
+        cout<<ansSize;
+        //even lenght
+        for(int i=0; i<n; i++){
+            int currSize = 0;
+            for(int j = i + 1, k = i; j<n && k>=0; j++, k--){
+                if(s[j] != s[k]) break;
+                currSize += 2;
+                if(currSize > ansSize){
+                    L = k;
+                    R = j;
+                    ansSize = currSize;
+                }
+            }
+        }
+        if(L == 0 && R == 0){
+            cout<<"entro";
+            return s.substr(L, R+1);
+        }
+        cout<<L<<R;
+        return s.substr(L, ansSize);
+    }
+};
